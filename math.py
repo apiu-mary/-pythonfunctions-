a19cf180a3d7b8f21d7d303d33a0d5d5358276d7
apiuuagou@gmail.com <apiuagou@gmail.com>
2023-03-31 15:55:30 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract (c,d):
+    answer1=c-d
+    return answer1
+
+def division(t,r):
+    results= t/r
+    return results
+def multiply(g,h):
+    results=g*h
+    return results
+
+def remainder(e,u):
+    results=e%u
+    return results 
